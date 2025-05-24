723c0c545db9780d9602e9a1b164d2e492dde1b8
boricsk <borics.krisztian@gmail.com>
2025-05-24 16:57:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+<p align="center">
+  <img src=https://devnullsec.hu/logo_small.svg height="64" width="64">
+</p>
+
+## RSA Secret Management
+### C# helper class for handling RSA Encryption and Decription
+
+
+- Usage
+> The class allows you to use the RSA encryption algorithm securely. Public and private keys are created in the Windows Certificate store, so the keys are properly protected. Example: 
+
+```csharp
+using RsaSecretManagement;
+namespace RsaDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Generate RSA keypair. It will save your windows certificate store (certmgr)
+            //The key pair generation should be run only once, not before each encryption/decryption.
+            //If a key pair with the specified name already exists, it will not be regenerated.
+            string myCertName = "MyFirstCert";
+            int validity = 100;
+            int keySize = 2048;
+            string plainText = "HelloRSA";
+            RsaSecretMgmnt.GenetateRSAKeyPair(myCertName, validity, keySize);
+            Console.WriteLine("RSA key pair generated...");
+
+            //2. Encrypt
+            var encryptedData = RsaSecretMgmnt.Encrypt(plainText, myCertName);
+            Console.WriteLine($"Encrypted : {encryptedData}");
+
+            //3. Decrypt
+            var decryptedData = RsaSecretMgmnt.Decrypt(encryptedData, myCertName);
+            Console.WriteLine($"Decrypted : {decryptedData}");
+        }
+    }
+}
+```
+<br>
+<p align="center">
+  <a href="https://www.nuget.org/packages/MifareClassic/1.0.0" target="_blank" >
+    <img src="./nuget_logo.png" alt="NuGet Page">
+  </a>
+</p>
---
+++ RsaSecretManagement/RsaSecretMgmnt.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace RsaSecretManagement
+{
+    public static class RsaSecretMgmnt
+    {
+        public static void GenetateRSAKeyPair(string certSubject, int validityInYears, int keySize = 4096)
+        {
+            if (GetCert(certSubject) == null)
+            {
+                using RSA rsa = RSA.Create(keySize);
+                string subject = $"CN={certSubject}";
+                var request = new CertificateRequest(
+                    subject,
+                    rsa,
+                    HashAlgorithmName.SHA256,
+                    RSASignaturePadding.Pkcs1);
+
+                X509Certificate2 cert = request.CreateSelfSigned(
+                    DateTimeOffset.Now,
+                    DateTimeOffset.Now.AddYears(validityInYears));
+
+                var certWithKey = new X509Certificate2(
+                    cert.Export(X509ContentType.Pfx),
+                    (string?)null,
+                    X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.UserKeySet);
+
+                using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(certWithKey);
+                store.Close();
+            }
+        }
+
+        public static X509Certificate2? GetCert(string certSubject)
+        {
+            using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            var cert = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, $"CN={certSubject}", false);
+            if (cert.Count() != 0)
+            {
+                return cert[0];
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public static byte[]? Encrypt(string plainText, string certSubject)
+        {
+            X509Certificate2? cert = GetCert(certSubject);
+
+            if (cert != null)
+            {
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                using RSA? publicRsa = cert.GetRSAPublicKey();
+                byte[] encrypted = publicRsa.Encrypt(plainBytes, RSAEncryptionPadding.Pkcs1);
+                return encrypted;
+            }
+            else { return null; }
+        }
+        public static string Decrypt(byte[] encryptedData, string certSubject)
+        {
+            X509Certificate2? cert = GetCert(certSubject);
+            if (cert != null)
+            {
+                using RSA? privateRsa = cert.GetRSAPrivateKey();
+                byte[] decrypted = privateRsa.Decrypt(encryptedData, RSAEncryptionPadding.Pkcs1);
+                string result = Encoding.UTF8.GetString(decrypted);
+                return result;
+            }
+            else { return string.Empty; }
+        }
+    }
+}
